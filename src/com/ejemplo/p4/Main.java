f50b25167f3b1924252ade2fee70b54bc37f798e
mmorejondev <mmorejondev@gmail.com>
2026-01-29 12:37:07 +0100

    Inicio Proyecto Práctica 4

---
+++ src/com/ejemplo/p4/Main.java
@@ -0,0 +1,10 @@
+package com.ejemplo.p4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 System.out.println("Practica 4 - Git casos reales");
+		 System.out.println("Miguel Ángel Morejón Montero");
+	}
+
+}

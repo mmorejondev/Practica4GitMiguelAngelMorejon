312283dff85fda71e9b6823d3153184b24b56b14
mmorejondev <mmorejondev@gmail.com>
2026-02-17 21:15:53 +0100

    Commit para perder y recuperar

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		 System.out.println("Practica 4 - Git casos reales");
 		 System.out.println("Miguel Ángel Morejón Montero");
+		 System.out.println("Mensaje añadido en el punto 9 - REFLOG");
 	}
 
 }

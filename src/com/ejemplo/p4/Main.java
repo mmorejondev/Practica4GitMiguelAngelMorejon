bffd445c1ecaf827d4de4482d6c9405089e6d447
mmorejondev <mmorejondev@gmail.com>
2026-02-17 08:43:54 +0100

    Revert "Cambio malo subido por error"
    This reverts commit ef218e51ab516d4d5c60916d40f10cca3964f3a6.

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		 System.out.println("Practica 4 - Git casos reales");
 		 System.out.println("Miguel Ángel Morejón Montero");
-		 System.out.println("Este es un mensaje absurdo... Punto 6.2");
 	}
 
 }
